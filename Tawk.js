fe91bdccb0f4821189215c5c647946b78346dd37
hun1001 <l01022899266@gmail.com>
2022-04-28 12:55:59 +0900

    코드 수정

--- Tawk.js
+++ Tawk.js
@@ -3,7 +3,6 @@ var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
     var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
     s1.async = true;
     s1.src = 'https://embed.tawk.to/626a0be4b0d10b6f3e6fb40e/1g1n3stf4';
-    s1.charset = 'UTF-8';
     s1.setAttribute('crossorigin', '*');
     s0.parentNode.insertBefore(s1, s0);
 })();
