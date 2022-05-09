fb3940ccf6951308b1955ce73381c6083878fa9c
hun1001 <l01022899266@gmail.com>
2022-05-09 09:39:19 +0900

    수업내용

--- syntax.js
+++ syntax.js
@@ -1,16 +1,44 @@
-console.log(1);
-console.log(1 + 1);
-console.log(2 - 1);
-console.log(2 * 2);
-console.log(4 / 2);
+// console.log(1);
+// console.log(1 + 1);
+// console.log(2 - 1);
+// console.log(2 * 2);
+// console.log(4 / 2);
 
-var str = '상빈아 생일 축하해';
-console.log(str);
+// var str = '상빈아 생일 축하해';
+// console.log(str);
 
-var name = '서린';
-str = `Officia veniam nostrud in ${name} \nnostrud esse nisi excepteur adipisicing labore. Incididunt ex nostrud fugiat laborum irure cillum ea do magna magna occaecat ut ex. Nostrud velit elit ullamco reprehenderit minim occaecat voluptate sit tempor ea incididunt magna. Ad aliquip ipsum commodo dolore consectetur sunt sint in velit nostrud exercitation commodo. Eu id officia elit esse exercitation eu officia nulla non. Nulla eu elit laborum mollit incididunt ea quis voluptate laborum quis eiusmod.`;
+// var name = '서린';
+// str = `Officia veniam nostrud in ${name} \nnostrud esse nisi excepteur adipisicing labore. Incididunt ex nostrud fugiat laborum irure cillum ea do magna magna occaecat ut ex. Nostrud velit elit ullamco reprehenderit minim occaecat voluptate sit tempor ea incididunt magna. Ad aliquip ipsum commodo dolore consectetur sunt sint in velit nostrud exercitation commodo. Eu id officia elit esse exercitation eu officia nulla non. Nulla eu elit laborum mollit incididunt ea quis voluptate laborum quis eiusmod.`;
 
-str = 'Officia veniam nostrud in ' + name + ' \nnostrud esse nisi excepteur adipisicing labore.\
-    \ Incididunt ex nostrud fugiat laborum irure cillum ea do magna magna occaecat ut ex. Nostrud velit elit ullamco reprehenderit minim occaecat voluptate sit tempor ea incididunt magna. Ad aliquip ipsum commodo dolore consectetur sunt sint in velit nostrud exercitation commodo. Eu id officia elit esse exercitation eu officia nulla non. Nulla eu elit laborum mollit incididunt ea quis voluptate laborum quis eiusmod.';
+// str = 'Officia veniam nostrud in ' + name + ' \nnostrud esse nisi excepteur adipisicing labore.\
+//     \ Incididunt ex nostrud fugiat laborum irure cillum ea do magna magna occaecat ut ex. Nostrud velit elit ullamco reprehenderit minim occaecat voluptate sit tempor ea incididunt magna. Ad aliquip ipsum commodo dolore consectetur sunt sint in velit nostrud exercitation commodo. Eu id officia elit esse exercitation eu officia nulla non. Nulla eu elit laborum mollit incididunt ea quis voluptate laborum quis eiusmod.';
 
-console.log(str);
+// console.log(str);
+
+// var e = 1;
+
+// console.log(e == true);
+// console.log(e === true);
+
+// var args = process.argv;
+
+// console.log(args);
+
+// if (args[2] == 'C1') {
+//     console.log('C1');
+// }
+// else console.log('C2');
+
+// while (true) {
+//     console.log('안녕');
+// }
+
+// for (var i = 0; i < 100; i++) {
+
+// }
+
+var arr1 = [1, 2, 3, 4, 5];
+var arr2 = new Array('A', 'B', 'C', 'D', 'E');
+
+console.log(arr1);
+console.log(arr2);
