0285312b22a0940f0ea54a1e5bbf89590432155f
hun1001 <l01022899266@gmail.com>
2022-05-09 09:48:05 +0900

    문법 오늘 배운거

--- syntax.js
+++ syntax.js
@@ -42,3 +42,29 @@ var arr2 = new Array('A', 'B', 'C', 'D', 'E');
 
 console.log(arr1);
 console.log(arr2);
+
+arr1.push(6);
+arr2.push('F');
+
+console.log(arr1);
+console.log(arr2);
+
+arr1.unshift(0);
+arr2.unshift(9);
+
+arr1.splice(1, 0, 7);
+
+console.log(arr1);
+console.log(arr2);
+
+arr1.splice(1, 1);
+arr2.splice(1, 1);
+
+console.log(arr1);
+console.log(arr2);
+
+var a = function () {
+    console.log('안녕');
+}
+
+a();
