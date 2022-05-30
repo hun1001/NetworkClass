d0619912d7f18aaa739caa8438719f3e89a0a214
hun1001 <l01022899266@gmail.com>
2022-05-30 09:26:06 +0900

    언제부턴가 수업을 듣기 싫어졌어 아니 코드를 왜 이렇게 짜는걸까 진짜로 머하는걸까 현타글잣

--- main_23.js
+++ main_23.js
@@ -2,26 +2,14 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 
-var app = http.createServer(function (request, response) {
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var title = queryData.id;
-  var pathname = url.parse(_url, true).pathname;
-
-  if (pathname == '/') {
-    if (title == undefined) {
-      fs.readdir('./data', function (err, filelist) {
-        title = 'HELLO';
-        var description = '메롱 첫번째 페이지';
-
-        var list = '<ol>';
-        for (var i = 0; i < filelist.length; i++) {
-          list = list + `<li><a href='/?id=${filelist[i]}'>${filelist[i]}</a></li>`
-        }
-        list = list + '</ol>';
-
-        var template = `
-          <!DOCTYPE html>
+function templateHTML(title, list, body) {
+  if (list == null) {
+    list = '<li>성찰1</li>';
+    list += '<li>성찰2</li>';
+    list += '<li>성찰3</li>';
+  }
+  return `
+    <!DOCTYPE html>
           <html lang="en" dir="ltr">
 
           <head>
@@ -34,10 +22,38 @@ var app = http.createServer(function (request, response) {
 
             ${list}
 
-            <p>${description}</p>
+            <p>${body}</p>
           </body>
           </html>
-          `;
+          
+    `;
+}
+
+var templateList = (filelist) => {
+  var list = `<ol>`;
+  for (var i = 0; i < filelist.length; i++) {
+    list = list + `<li><a href='/?id=${filelist[i]}'>${filelist[i]}</a></li>`
+  }
+  list = list + '</ol>';
+}
+
+
+var app = http.createServer(function (request, response) {
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var title = queryData.id;
+  var pathname = url.parse(_url, true).pathname;
+  var func = templateHTML;
+
+  if (pathname == '/') {
+    if (title == undefined) {
+      fs.readdir('./data', function (err, filelist) {
+        title = 'HELLO';
+        var description = '메롱 첫번째 페이지';
+
+        var list = templateList(filelist);
+
+        var template = templateHTML(title, list, description);
         response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         response.end(template);
       })
@@ -49,30 +65,8 @@ var app = http.createServer(function (request, response) {
         var description = '메롱 첫번째 페이지';
         fs.readdir('./data', function (err, filelist) {
           title = 'HELLO';
-          for (var i = 0; i < filelist.length; i++) {
-            list = list + `<li><a href='/?id=${filelist[i]}'>${filelist[i]}</a></li>`
-          }
-          list = list + '</ol>';
         });
-        var template = `
-          <!DOCTYPE html>
-          <html lang="en" dir="ltr">
-
-          <head>
-            <meta charset="utf-8">
-            <title> ${title} </title>
-          </head>
-
-          <body>
-            <h1> <a href="/"> ${title}</a> </h1>
-
-            <ol>
-                ${list}
-            </ol>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
+        var template = templateHTML(title, list, description);
         response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         response.end(template);
       });
@@ -85,27 +79,7 @@ var app = http.createServer(function (request, response) {
   }
 
   fs.readFile(`data/${queryData.id}`, 'utf8', function (err, description) {
-    var template = `
-      <!DOCTYPE html>
-      <html lang="en" dir="ltr">
-
-      <head>
-        <meta charset="utf-8">
-        <title> ${title} </title>
-      </head>
-
-      <body>
-        <h1> <a href="/"> ${title}</a> </h1>
-
-        <ol>
-            <li> <a href ="/?id=성찰교실1">목록1</a> </li>
-            <li> <a href ="/?id=성찰교실2">목록2</a> </li>
-            <li> <a href ="/?id=성찰교실3">목록3</a> </li>
-        </ol>
-        <p>${description}</p>
-      </body>
-      </html>
-      `;
+    var template = templateHTML(title, list, description);
     response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     response.end(template);
 
